4c9c66d222ba4a668555d759c2b83fdaea55b5ce
ynn <yoann.maurel@irisa.fr>
2020-04-30 10:02:57 +0200

    test

--- docker/cli.sh
+++ docker/cli.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker run -p 5000:5000 -v $(pwd):/work -it --rm nnynn/xtdgift /app/xtdgift.py
+sudo docker run -p 5000:5000 -v $(pwd):/work -it --rm nnynn/xtdgift /app/xtdgift.py $@
